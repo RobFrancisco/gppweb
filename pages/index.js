6940a3c36d605b23f0c8f9dbdc93da3e1ccae4f0
RobFrancisco <robernestfrancisco@gmail.com>
2025-09-14 01:30:50 +0800

    gpp commit

---
+++ pages/index.js
@@ -0,0 +1,22 @@
+import Image from "next/image"
+import Navbar from "./navbar"
+import Footer from "./Footer"
+import { Geist, Geist_Mono } from "next/font/google";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Milan</h1>
+    </div>
+  );
+}
